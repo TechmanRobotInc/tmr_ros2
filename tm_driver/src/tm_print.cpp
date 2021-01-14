37a2f5a1b1ac6117131ed0663c5873b4d63ccf94
leo.wu <leo.wu@tm-robot.com>
2021-01-14 15:09:43 +0000

    1.3.0  change print system and use color to seperate messages

--- tm_driver/src/tm_print.cpp
+++ tm_driver/src/tm_print.cpp
@@ -4,7 +4,8 @@
 #include "tm_driver/tm_print.h"
 #endif
 
-#include <stdio.h>
+//#include <stdio.h>
+#include "iostream"
 #include <stdarg.h>
 
 #ifdef _WIN32
@@ -22,22 +23,35 @@
 
 #define MAX_MSG_SIZE 256
 
+void (*print_debug_function)(char* fmt);
+bool isSetPrintDebugFunction = false;
+
+void default_debug_function_print(char* msg){
+  std::cout<<PRINT_CYAN<<"[DEBUG] "<<msg<<std::endl<<PRINT_RESET;
+}
+
 int print_debug(const char* fmt, ...) {
+
+	
 	char msg[MAX_MSG_SIZE];
 	int n;
 	va_list vl;
 	va_start(vl, fmt);
 	n = vsnprintf_s(msg, MAX_MSG_SIZE, fmt, vl);
 	va_end(vl);
-#ifdef ROS_BUILD
-	ROS_DEBUG("%s", msg);
-#else
-#ifdef ROS2_BUILD
-#else
-	printf("[DEBUG] %s\n", msg);
-#endif
-#endif
-	return n;
+  if(isSetPrintDebugFunction){
+    print_debug_function(msg);
+  } else{
+    default_debug_function_print(msg);
+  }
+  return n;
+}
+
+void (*print_info_function)(char* fmt);
+bool isSetPrintInfoFunction = false;
+
+void default_print_info_function_print(char* msg){
+  std::cout<<"[INFO] "<<msg<<std::endl;
 }
 
 int print_info(const char* fmt, ...) {
@@ -47,18 +61,20 @@ int print_info(const char* fmt, ...) {
 	va_start(vl, fmt);
 	n = vsnprintf_s(msg, MAX_MSG_SIZE, fmt, vl);
 	va_end(vl);
-#ifdef ROS_BUILD
-	ROS_INFO("%s", msg);
-#else
-#ifdef ROS2_BUILD
-	printf("[INFO] [tm_driver]: %s\n", msg);
-#else
-	printf("[ INFO] %s\n", msg);
-#endif
-#endif
+    if(isSetPrintInfoFunction){
+        print_info_function(msg);
+    } else{
+        default_print_info_function_print(msg);
+    }
 	return n;
 }
 
+void (*print_warn_function)(char* fmt);
+bool isSetPrintWarnFunction = false;
+
+void default_print_warn_function_print(char* msg){
+  std::cout<<PRINT_YELLOW<<"[WARN] "<<msg<<std::endl<<PRINT_RESET;
+}
 int print_warn(const char* fmt, ...) {
 	char msg[MAX_MSG_SIZE];
 	int n;
@@ -66,18 +82,21 @@ int print_warn(const char* fmt, ...) {
 	va_start(vl, fmt);
 	n = vsnprintf_s(msg, MAX_MSG_SIZE, fmt, vl);
 	va_end(vl);
-#ifdef ROS_BUILD
-	ROS_WARN("%s", msg);
-#else
-#ifdef ROS2_BUILD
-	printf("[WARN] [tm_driver]: %s\n", msg);
-#else
-	printf("[ WARN] %s\n", msg);
-#endif
-#endif
+    if(isSetPrintWarnFunction){
+		print_warn_function(msg);
+	} else{
+		default_print_warn_function_print(msg);
+	}
+	
 	return n;
 }
 
+void (*print_error_function)(char* fmt);
+bool isSetPrintErrorFunction = false;
+
+void default_print_error_function_print(char* msg){
+  std::cout<<PRINT_RED<<"[ERROR] "<<msg<<std::endl<<PRINT_RESET;
+}
 int print_error(const char* fmt, ...) {
 	char msg[MAX_MSG_SIZE];
 	int n;
@@ -85,18 +104,20 @@ int print_error(const char* fmt, ...) {
 	va_start(vl, fmt);
 	n = vsnprintf_s(msg, MAX_MSG_SIZE, fmt, vl);
 	va_end(vl);
-#ifdef ROS_BUILD
-	ROS_ERROR("%s", msg);
-#else
-#ifdef ROS2_BUILD
-	printf("[ERROR] [tm_driver]: %s\n", msg);
-#else
-	printf("[ERROR] %s\n", msg);
-#endif
-#endif
+    if(isSetPrintErrorFunction){
+		print_error_function(msg);
+	} else{
+		default_print_error_function_print(msg);
+	}
 	return n;
 }
 
+void (*print_fatal_function)(char* fmt);
+bool isSetPrintFatalFunction = false;
+
+void default_print_fatal_function_print(char* msg){
+  std::cout<<PRINT_GREEN<<"[FATAL] "<<msg<<std::endl<<PRINT_RESET;
+}
 int print_fatal(const char* fmt, ...) {
 	char msg[MAX_MSG_SIZE];
 	int n;
@@ -104,15 +125,32 @@ int print_fatal(const char* fmt, ...) {
 	va_start(vl, fmt);
 	n = vsnprintf_s(msg, MAX_MSG_SIZE, fmt, vl);
 	va_end(vl);
-#ifdef ROS_BUILD
-	ROS_FATAL("%s", msg);
-	ros::shutdown();
-#else
-#ifdef ROS2_BUILD
-	printf("[FATAL] [tm_driver]: %s\n", msg);
-#else
-	printf("[FATAL] %s\n", msg);
-#endif
-#endif
+    if(isSetPrintErrorFunction){
+		print_fatal_function(msg);
+	} else{
+		default_print_fatal_function_print(msg);
+	}
 	return n;
+
+}
+
+void set_up_print_debug_function(void (*function_print)(char* fmt)){
+  print_debug_function = function_print;
+  isSetPrintDebugFunction = true;
+}
+void set_up_print_info_function(void (*function_print)(char* fmt)){
+  print_info_function = function_print;
+  isSetPrintInfoFunction = true;
+}
+void set_up_print_warn_function(void (*function_print)(char* fmt)){
+  print_warn_function = function_print;
+  isSetPrintWarnFunction = true;
+}
+void set_up_print_error_function(void (*function_print)(char* fmt)){
+  print_error_function = function_print;
+  isSetPrintErrorFunction = true;
+}
+void set_up_print_fatal_function(void (*function_print)(char* fmt)){
+  print_fatal_function = function_print;
+  isSetPrintFatalFunction = true;
 }
