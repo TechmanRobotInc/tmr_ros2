4a57c21aecbc4e91b556d3429d647cf38a527133
leo.wu <leo.wu@tm-robot.com>
2021-02-04 15:10:12 +0000

    1.3.0 add print color and  modify the ip setting piture to fix tutorial

--- tm_driver/src/tm_ros2_composition.cpp
+++ tm_driver/src/tm_ros2_composition.cpp
@@ -3,10 +3,35 @@
 
 #include "rclcpp/rclcpp.hpp"
 
+void debug_function_print(char* msg){
+  printf("%s[TM_DEBUG] %s\n%s", PRINT_CYAN.c_str(), msg, PRINT_RESET.c_str());
+}
+void info_function_print(char* msg){
+  printf("[TM_INFO] %s\n", msg);
+}
+void warn_function_print(char* msg){
+  printf("%s[TM_WARN] %s\n%s", PRINT_YELLOW.c_str(), msg, PRINT_RESET.c_str());
+}
+void error_function_print(char* msg){
+  printf("%s[TM_ERROR] %s\n%s", PRINT_RED.c_str(), msg, PRINT_RESET.c_str());
+}
+void fatal_function_print(char* msg){
+  printf("%s[TM_FATAL] %s\n%s", PRINT_GREEN.c_str(), msg, PRINT_RESET.c_str());
+}
+void set_up_print_fuction(){
+  set_up_print_debug_function(debug_function_print);
+  set_up_print_info_function(info_function_print);
+  set_up_print_warn_function(warn_function_print);
+  set_up_print_error_function(error_function_print);
+  set_up_print_fatal_function(fatal_function_print);
+}
+
 int main(int argc, char *argv[])
 {
     // Force flush of the stdout buffer.
     setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+    
+    set_up_print_fuction();
 
     rclcpp::init(argc, argv);
     
