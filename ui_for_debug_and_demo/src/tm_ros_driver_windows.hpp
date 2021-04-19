2f202e53ca9b9f34b5fdd73184a306eb3337c578
YH.Wang <yh.wang@tm-robot.com>
2021-04-19 10:40:48 +0800

    1.6.1 update TM GUI debugging GUI and README.md .

--- ui_for_debug_and_demo/src/tm_ros_driver_windows.hpp
+++ ui_for_debug_and_demo/src/tm_ros_driver_windows.hpp
@@ -1,31 +1,50 @@
+/*****************************************************************************
+** Ifdefs
+*****************************************************************************/
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
-#include <QMessageBox>
-#include <QFileDialog>
-#include <QStringListModel>
-#include <QButtonGroup>
-#include <QThread>
-#include <QStandardItemModel>
+/*****************************************************************************
+** Includes
+*****************************************************************************/
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
+#include <string>
+
+#include <QMainWindow>
+#include <QMessageBox>
+#include <QStringListModel>
+#include <QThread>
+#include <QStandardItemModel>
+
 #include "tm_msgs/msg/feedback_state.hpp"
-#include "tm_msgs/srv/connect_tm.hpp"
-#include "tm_msgs/srv/set_io.hpp"
 #include "tm_msgs/msg/sct_response.hpp"
 #include "tm_msgs/msg/svr_response.hpp"
 #include "tm_msgs/msg/sta_response.hpp"
+#include "tm_msgs/srv/connect_tm.hpp"
+#include "tm_msgs/srv/set_io.hpp"
 #include <ctime>
 
 using std::chrono::system_clock;
+/*****************************************************************************
+** Namespaces
+*****************************************************************************/
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+/*****************************************************************************
+** Class
+*****************************************************************************/
 class RosPage : public QThread {
  Q_OBJECT
- private:
+ 
+public:
+  RosPage(std::string nodeName);
+  ~RosPage();
+  void run();
+
+private:
   rclcpp::Node::SharedPtr node;
   rclcpp::Subscription<tm_msgs::msg::FeedbackState>::SharedPtr feedBackStatusSubscription;
   rclcpp::Subscription<tm_msgs::msg::SctResponse>::SharedPtr sctResponseSubscription;
@@ -33,55 +52,62 @@ class RosPage : public QThread {
   rclcpp::Subscription<tm_msgs::msg::SvrResponse>::SharedPtr svrResponseSubscription;
   rclcpp::Client<tm_msgs::srv::ConnectTM>::SharedPtr connectSvrClient;
   rclcpp::Client<tm_msgs::srv::ConnectTM>::SharedPtr connectSctClient;
-  bool lastStatus;
-  void initial_subscriber();
-  void initial_client();
   void feedback_states_callback(tm_msgs::msg::FeedbackState::SharedPtr msg);
-  void send_service(rclcpp::Client<tm_msgs::srv::ConnectTM>::SharedPtr client, std::shared_ptr<tm_msgs::srv::ConnectTM::Request> request);
-  std::string current_time();
   void sct_response_callback(tm_msgs::msg::SctResponse::SharedPtr msg);
   void sta_response_callback(tm_msgs::msg::StaResponse::SharedPtr msg);
   void svr_response_callback(tm_msgs::msg::SvrResponse::SharedPtr msg);
- signals:
+  void send_service(rclcpp::Client<tm_msgs::srv::ConnectTM>::SharedPtr client, std::shared_ptr<tm_msgs::srv::ConnectTM::Request> request);
+  bool lastStatus;
+  void initial_subscriber();
+  void initial_client();
+  std::string current_time();
+  
+signals:
   void send_ui_feed_back_status(tm_msgs::msg::FeedbackState::SharedPtr msg);
   void send_to_ui_list(std::string);
- private slots:
+  
+private slots:
   void send_sct_as_re_connect();
   void send_svr_as_re_connect();
   void change_control_box_io_button();
- public:
-  RosPage(std::string nodeName);
-  ~RosPage();
-  void run();
+  
 };
 
 class MainWindow : public QDialog {
   Q_OBJECT
- private:
+
+public:
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
+
+private:
   Ui::MainWindow *ui;
   std::unique_ptr<RosPage> rosPage;
   std::unique_ptr<QStandardItemModel> statusItemModel;
-  int testCount = 0;
   int rowIndex;
   void initial_ros_thread_to_ui_page();
   void initial_ui_page_to_ros_thread();
-  void set_text_true_false(bool isTrue, QLabel* label);
-  void initial_ui_compoment();
-  QString formate_change(std::string msg);
- signals:
+  void set_text_nan_initial(QLabel* label);    
+  void set_text_true_false(bool isTrue, QLabel* label, bool isReverse);
+  void set_text_on_off(bool isTrue, QLabel* label, bool isReverse);
+  void set_text_high_low(bool isTrue, QLabel* label, bool isReverse);
+  void set_text_null_reserve(bool isTrue, QLabel* label);
+  void initial_text_ctrl_label();      
+  void initial_ui_component();
+  QString format_change(std::string msg);
+  
+signals:
   void send_sct_as_re_connect();
   void send_svr_as_re_connect();
   void change_control_box_io_button();
- private slots:
+
+private slots:
   void send_ui_feed_back_status(tm_msgs::msg::FeedbackState::SharedPtr msg);
   void send_to_ui_list(std::string);
   void click_set_sct_re_connect_button();
   void click_set_svr_re_connect_button();
   void click_change_control_box_io_button();
   void click_clear_response_button();
- public:
-  MainWindow(QWidget *parent = nullptr);
-  ~MainWindow();
 
 };
 #endif // MAINWINDOW_H
