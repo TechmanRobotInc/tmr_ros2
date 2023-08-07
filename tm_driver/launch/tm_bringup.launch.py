69fcc6e3a4760858a411771564c6d3abdd5e5cc5
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-08-07 17:56:38 +0200

    simplify getting args

--- tm_driver/launch/tm_bringup.launch.py
+++ tm_driver/launch/tm_bringup.launch.py
@@ -2,14 +2,9 @@ import sys
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
-    args = []
-    length = len(sys.argv)
-    if (len(sys.argv) >= 5):
-        i = 4
-        while i < len(sys.argv):
-            args.append(sys.argv[i])
-            i = i + 1
+    args = sys.argv[4:]
 
     return LaunchDescription([
         Node(
