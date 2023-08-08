c23491c523386d3a3e277664e6446c11a975ccf9
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-08-08 16:06:20 +0200

    (demo) fix FutureReturnCode namespacing

--- demo/src/demo_ask_item.cpp
+++ demo/src/demo_ask_item.cpp
@@ -31,7 +31,7 @@ int main(int argc, char **argv)
   auto result = client->async_send_request(request);
   // Wait for the result.
   if (rclcpp::spin_until_future_complete(node, result) ==
-    rclcpp::executor::FutureReturnCode::SUCCESS)
+    rclcpp::FutureReturnCode::SUCCESS)
   {
     if(result.get()->ok){
       RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"),"OK");
