9b8416c9feef9d58c7edd79ce6424b6bcd934097
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-08-10 09:55:37 +0200

    (tm_driver) restore ip parsing

--- tm_driver/src/tm_ros2_composition.cpp
+++ tm_driver/src/tm_ros2_composition.cpp
@@ -63,15 +63,20 @@ int main(int argc, char *argv[])
     set_up_ros_print_fuction();
 
     std::vector<std::string> args = rclcpp::init_and_remove_ros_arguments(argc, argv);
-    
-    if (args.size() < 2)
-    {
+
+    std::string host;
+    if (args.size() > 1) {
+        host = args[1];
+        if (host.find("robot_ip:=") != std::string::npos) {
+            host.replace(host.begin(), host.begin() + 10, "");
+        } else if (host.find("ip:=") != std::string::npos) {
+            host.replace(host.begin(), host.begin() + 4, "");
+        }
+    } else {
         print_fatal("Please provide the 'robot_ip_address'");
         rclcpp::shutdown();
         return 1;
     }
-
-    const std::string& host = args[1];
 
     if (args.size() == 3){
       bool isSetNoLogPrint;
--- tm_driver/src/tm_ros2_composition_moveit.cpp
+++ tm_driver/src/tm_ros2_composition_moveit.cpp
@@ -73,18 +73,29 @@ int main(int argc, char *argv[])
       return 1;
     }
 
-    const std::string& host = args[1];
+    bool is_fake = true;
+    std::string host;
+    if (argc > 1) {
+      host = args[1];
+      if (host.find("robot_ip:=") != std::string::npos) {
+        host.replace(host.begin(), host.begin() + 10, "");
+        is_fake = false;
+      } else if (host.find("ip:=") != std::string::npos) {
+        host.replace(host.begin(), host.begin() + 4, "");
+        is_fake = false;
+      } else{
+        print_info("ip is not found, using a fake robot");
+      }
+    } else {
+      print_fatal("Please provide the 'robot_ip_address'");
+      rclcpp::shutdown();
+      return 1;
+    }
 
-    bool is_fake = false;
-    // Regex logic taken from https://www.geeksforgeeks.org/how-to-validate-an-ip-address-using-regex/
-    std::regex ipv4("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])");
-    std::regex ipv6("((([0-9a-fA-F]){1,4})\\:){7}([0-9a-fA-F]){1,4}");
-
-    if (!std::regex_match(host, ipv4) && !std::regex_match(host, ipv6)) {
+    if (is_fake) {
       std::stringstream ss;
-      ss << "ip '" << host << "' is not a valid ip-address, using a fake robot";
+      ss << "Only ip or robot_ip support, but your type is '" << host << "', using a fake robot";
       print_info(ss.str().c_str());
-      is_fake = true;
     }
 
     if (args.size() == 3){
