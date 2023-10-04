bde585e1ee13c45f4c9e3066a6a164ec4def0f24
Matthijs van der Burgh <matthijs.van.der.burgh@vbti.nl>
2023-10-04 10:16:01 +0200

    (tm_driver) additional goal_time_tolerance of 19xtotal_time
    As project speed can't be taken into account for now

--- tm_driver/src/tm_ros2_movit_sct.cpp
+++ tm_driver/src/tm_ros2_movit_sct.cpp
@@ -118,7 +118,7 @@ TmRos2SctMoveit::execute_traj(const std::shared_ptr<rclcpp_action::ServerGoalHan
   if (!is_fake_) {
 //    print_info("TM_ROS: traj. goal_time_tolerance:= %.3f", sec(goal_handle->get_goal()->goal_time_tolerance));
 //    iface_.run_pvt_traj(*pvts, sec(goal_handle->get_goal()->goal_time_tolerance));
-    iface_.run_pvt_traj(*pvts, 0.5*pvts->total_time);
+    iface_.run_pvt_traj(*pvts, 19*pvts->total_time);
   }
   else {
     iface_.fake_run_pvt_traj(*pvts);
